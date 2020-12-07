ce5b236829b1db828a3188e7d3cb4caaab8675ba
John Lam <jlam@iunknown.com>
2020-12-07 08:34:11 -0800

    committing interactive script

--- script2.py
+++ script2.py
@@ -1,110 +1,10 @@
-# Another take on the code, partially refactored into teslatypes.py
+# Another take on the code, partially refactored into teslahunter.py
+
+# %%
+import teslahunter as tt
 
 #%%
 
-# Custom enum types to make the options work better
-
-import enum
-
-class EnumValue:
-    value : str
-    description : str 
-
-    def __init__(self, value : str, description : str):
-        self.value = value 
-        self.description = description
-
-class ExteriorColor(enum.Enum):
-    RED = EnumValue('RED', 'Red Multi-Coat')
-    WHITE = EnumValue('WHITE', 'Pearl White Multi-Coat')
-    SILVER = EnumValue('SILVER', 'Silver Metallic')
-    BLUE = EnumValue('BLUE', 'Deep Blue Metallic')
-    BLACK = EnumValue('BLACK', 'Solid Black')
-    GRAY = EnumValue('GRAY', 'Midnight Silver Metallic')
-    UNKNOWN = EnumValue('UNKNOWN', 'Unknown Exterior Color')
-
-class InteriorColor(enum.Enum):
-    BLACK = EnumValue('PREMIUM_BLACK', 'Premium Black')
-    WHITE = EnumValue('PREMIUM_WHITE', 'Premium Black and White')
-    UNKNOWN = EnumValue("UNKNOWN", 'Unknown Interior Color')
-
-class Drivetrain(enum.Enum):
-    LRAWD = EnumValue('LRAWD', "Long Range AWD")
-    LRAWDP = EnumValue('LRAWDP', "Long Range AWD Performance")
-    LRRWD = EnumValue('LRRWD', "Long Range RWD")
-    MRRWD = EnumValue('MRRWD', "Medium Range RWD")
-    SRPRWD = EnumValue('SRPRWD', "Standard Range Plus RWD")
-    UNKNOWN = EnumValue("UNKNOWN", 'Unknown Drivetrain')
-
-class Wheels(enum.Enum):
-    EIGHTEEN = EnumValue('EIGHTEEN', '18" Aero Wheels')
-    NINETEEN = EnumValue('NINETEEN', '19" Sport Wheels')
-    TWENTY = EnumValue('TWENTY', '20" Fragile Wheels')
-    UNKNOWN = EnumValue('Unknown', "Unknown Wheel Type")
-
-# Return descriptions for UX
-
-def get_descriptions(t):
-    return [v.value.description for v in t]
-
-# Lookup an Enum value based on the description
-
-def get_value(t, description : str):
-    for e in t:
-        if e.value.description == description:
-            return e
-    return t.UNKNOWN # this is by convention
-
-# %%
-
-import enum
-
-class ExteriorColor(enum.Enum):
-    RED = 'Red Multi-Coat'
-    WHITE = 'Pearl White Multi-Coat'
-    SILVER = 'Silver Metallic'
-    BLUE = 'Deep Blue Metallic'
-    BLACK = 'Solid Black'
-    GRAY = 'Midnight Silver Metallic'
-    UNKNOWN = 'Unknown Exterior Color'
-
-class InteriorColor(enum.Enum):
-    PREMIUM_BLACK = 'Premium Black'
-    PREMIUM_WHITE = 'Premium Black and White'
-    UNKNOWN = 'Unknown Interior Color'
-
-class Drivetrain(enum.Enum):
-    LRAWD = 'Long Range AWD'
-    LRAWDP = 'Long Range AWD Performance'
-    LRRWD = 'Long Range RWD'
-    MRRWD = 'Medium Range RWD'
-    SRPRWD = 'Standard Range Plus RWD'
-    UNKNOWN = 'Unknown Drivetrain'
-
-class Wheels(enum.Enum):
-    EIGHTEEN = '18" Aero Wheels'
-    NINETEEN = '19" Sport Wheels'
-    TWENTY = '20" Fragile Wheels'
-    UNKNOWN = "Unknown Wheel Type"
-
-# Return descriptions for UX
-
-def get_descriptions(t : enum.Enum):
-    return [v.value for v in t]
-
-# Lookup an Enum value based on the description
-
-def get_value(t : enum.Enum, description : str):
-    for e in t:
-        if e.value == description:
-            return e
-    print(f"Unknown {description} while looking up {t}")
-    return t.UNKNOWN # this is by convention
-
-# %%
-import teslatypes as tt
-
-#%%
 import importlib 
 importlib.reload(tt)
 
@@ -117,5 +17,67 @@ query = tt.create_query(drivetrains, exterior_colors, interior_colors, wheels)
 cars = tt.query_tesla(query)
 df = tt.to_dataframe(cars)
 df
+
+# %%
+from datetime import date
+print(date.today())
+# %%
+
+import time 
+import teslahunter as t 
+
+#%%
+import importlib 
+importlib.reload(t)
+t.get_daily_data()
+
+# %%
+import pandas as pd
+df = pd.read_pickle(f"{date.today()}.pkl")
+rm = df.sample(5)
+df.drop(rm.index)
+# %%
+# TODO: this reload functionality really needs to be part of the tool, perhaps as an option?
+importlib.reload(t)
+
+df = pd.read_pickle("2020-11-25.pkl")
+days = t.synthesize_data(df)
+
+# %%
+
+df = pd.read_pickle("2020-11-25.pkl")
+
+days = 10
+add_per_day = 10
+sold_per_day = 5
+price_adjustment = 350
+
+dfs = [df]
+current_df = df
+for _ in range(days):
+    rows = len(current_df.index)
+    new_df = current_df.sample(frac=((rows - add_per_day)/rows))
+    new_df['Price'] = new_df['Price'] - price_adjustment
+    dfs.insert(0, new_df)
+    current_df = new_df
+
+for df in dfs:
+    print(len(df.index))
+
+# Simulate selling 5 cars per day
+# For each day, sample 5 cars to remove and add them to cars to remove list
+# Remove them from that day's list
+# Sample 5 more cars, add them to the remove list and remove 10 cars 
+# Repeat
+
+sold_list = dfs[0].sample(sold_per_day).index.tolist()
+for i in range(1, len(dfs)):
+    dfs[i] = dfs[i].drop(sold_list)
+    newly_sold = dfs[i].sample(sold_per_day).index.tolist()
+    sold_list += newly_sold
+
+for df in dfs:
+    print(len(df.index))
+# %%
 
 # %%
